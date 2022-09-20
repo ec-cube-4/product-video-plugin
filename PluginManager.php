533a583923a1bc14bc2eedc30fbccf617253ebdd
hoandd <hoandd@beetsoft.vn>
2022-09-20 16:55:16 +0700

    Add block to dtb_block

--- PluginManager.php
+++ PluginManager.php
@@ -4,11 +4,14 @@ namespace Plugin\ProductVideo4;
 use Eccube\Plugin\AbstractPluginManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Filesystem\Filesystem;
-use Eccube\Entity\Page;
+use Eccube\Entity\Block;
+use Eccube\Entity\Master\DeviceType;
 
 class PluginManager extends AbstractPluginManager
 {
-    private $originalDir = __DIR__.'/Resource/template/default/';
+    private $originalDir = __DIR__.'/Resource/template/front/';
+    
+    private $file_name = 'product_video';
 
     private $template = 'product_video.twig';
 
@@ -19,13 +22,14 @@ class PluginManager extends AbstractPluginManager
     public function enable(array $meta, ContainerInterface $container)
     {
         $this->copyBlock($container);
-        // /** @var EntityManagerInterface $entityManager */
-        // $entityManager = $container->get('doctrine')->getManager();
-        // $PageLayout = $entityManager->getRepository(Page::class)->findOneBy(['url' => 'plugin_coupon_shopping']);
-        // if (is_null($PageLayout)) {
-        //     // pagelayoutの作成
-        //     // $this->createPageLayout($container);
-        // }
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $container->get('doctrine')->getManager();
+        $Block = $entityManager->getRepository(Block::class)->findOneBy([
+            'file_name' => 'ProductVideo4/' . $this->file_name]
+        );
+        if (is_null($Block)) {
+            $this->createBlock($container);
+        }
     }
 
     /**
@@ -39,7 +43,59 @@ class PluginManager extends AbstractPluginManager
         // ファイルコピー
         $file = new Filesystem();
         // ブロックファイルをコピー
-        $file->copy($this->originalDir.$this->template, $templateDir.'/ProductVideo4/default/'.$this->template);
+        $file->copy($this->originalDir.$this->template, $templateDir.'/Block/ProductVideo4/'.$this->template);
     }
 
+    private function createBlock(ContainerInterface $container)
+    {
+        // ページレイアウトにプラグイン使用時の値を代入
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $container->get('doctrine')->getManager();
+
+        $DeviceType = $entityManager->getRepository(DeviceType::class)
+            ->find(DeviceType::DEVICE_TYPE_PC);
+
+        // Create block
+        $Block = new Block();
+        $Block->setDeviceType($DeviceType)
+            ->setName('product video')
+            ->setFileName('ProductVideo4/' . $this->file_name)
+            ->setUseController(false)
+            ->setDeletable(false);
+
+        $entityManager->persist($Block);
+        $entityManager->flush($Block);
+    }
+
+    /**
+     * Remove block.
+     *
+     * @param ContainerInterface $container
+     */
+    private function removeBlock(ContainerInterface $container)
+    {
+        $templateDir = $container->getParameter('eccube_theme_front_dir');
+        $file = new Filesystem();
+        $file->remove($templateDir.'/Block/ProductVideo4/' . $this->template);
+    }
+
+    /**
+     * @param array $meta
+     * @param ContainerInterface $container
+     */
+    public function disable(array $meta, ContainerInterface $container)
+    {
+        $this->removeBlock($container);
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $container->get('doctrine')->getManager();
+        $Block = $entityManager->getRepository(Block::class)->findOneBy([
+            'file_name' => 'ProductVideo4/'.$this->file_name
+        ]);
+        
+        if ($Block) {
+            // Remove block 
+            $entityManager->remove($Block);
+            $entityManager->flush();
+        }
+    }
 }
